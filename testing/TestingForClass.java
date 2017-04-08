5377ce86aee1bbb94af736b4917302f8efa971a3
deenski <deenski@krondor.me>
2017-04-08 09:28:03 -0600

    testing gitignore

---
+++ testing/TestingForClass.java
@@ -0,0 +1,12 @@
+// Jakob Vendegna
+// CIS 161
+// date 
+// description
+
+public class TestingForClass 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Testing gitignore");
+	}
+}
